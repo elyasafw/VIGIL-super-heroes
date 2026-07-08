1fe5604f62b07af3d6ccee60571bb5a7e895cc77
elyasaf waisman <elyasafw1997@gmail.com>
2026-07-08 08:53:45 +0300

    create checking health server func

--- controllers/heroController.js
+++ controllers/heroController.js
@@ -7,6 +7,20 @@ import {
     getHeroesByQuery,
 } from "../services/heroService.js";
 
+export async function checkHealthServer(req, res) {
+    try {
+        const healthReport = {
+            status: "UP",
+            message: "VIGIL Server is healthy and running",
+            time: new Date().toISOString(),
+        };
+        res.end(JSON.stringify(healthReport));
+    } catch (error) {
+        res.statusCode = 500;
+        res.end(JSON.stringify({ status: "DOWN", error: error.message }));
+    }
+}
+
 export async function getHeroes(req, res) {
     try {
         const allHeroes = await getHeroesByQuery(req.query);
